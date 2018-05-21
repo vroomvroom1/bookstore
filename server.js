d5bccb3fd5c91b587eb8b5bf988b6022f35850ed
Matt <mattvroom1@gmail.com>
2018-05-21 18:40:44 -0500

    updates

--- server.js
+++ server.js
@@ -26,101 +26,97 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
 //       $qProvider.errorOnUnhandledRejections(false);
 //   });
 
-app.get('/', function(req, res){
-  res.send('Please use /api/books or /api/genres');
+app.get('/', (req, res) => {
+	res.send('Please use /api/books or /api/genres');
 });
 
-// Connect to Routes, requests genres
-
-app.get('/api/genres', function(req, res){
-    Genre.getGenres(function(err, genres){
-      if(err){
-        throw err;
-      }
-      res.json(genres);
-    });
+app.get('/api/genres', (req, res) => {
+	Genre.getGenres((err, genres) => {
+		if(err){
+			throw err;
+		}
+		res.json(genres);
+	});
 });
 
-app.post('/api/genres', function(req, res){
-    let genre = req.body;
-    Genre.addGenre(genre, function(err, genre){
-      if(err){
-        throw err;
-      }
-      res.json(genre);
-    });
+app.post('/api/genres', (req, res) => {
+	var genre = req.body;
+	Genre.addGenre(genre, (err, genre) => {
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
 });
 
-app.put('/api/genres/:_id', function(req, res){
-  let id = req.params._id;
-    let genre = req.body;
-    Genre.updateGenre(id, genre, {}, function(err, genre){
-      if(err){
-        throw err;
-      }
-      res.json(genre);
-    });
+app.put('/api/genres/:_id', (req, res) => {
+	var id = req.params._id;
+	var genre = req.body;
+	Genre.updateGenre(id, genre, {}, (err, genre) => {
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
 });
 
-app.delete('/api/genres/:_id', function(req, res){
-  let id = req.params._id;
-    Genre.deleteGenre(id, function(err, genre){
-      if(err){
-        throw err;
-      }
-      res.json(genre);
-    });
+app.delete('/api/genres/:_id', (req, res) => {
+	var id = req.params._id;
+	Genre.removeGenre(id, (err, genre) => {
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
 });
 
-//Requests Books
-
-app.get('/api/books', function(req, res){
-    Book.getBooks(function(err, books){
-      if(err){
-        throw err;
-      }
-      res.json(books);
-    });
+app.get('/api/books', (req, res) => {
+	Book.getBooks((err, books) => {
+		if(err){
+			throw err;
+		}
+		res.json(books);
+	});
 });
 
-app.get('/api/books/:_id', function(req, res){
-    Book.getBookById(req.params._id, function(err, book){
-      if(err){
-        throw err;
-      }
-      res.json(book);
-    });
+app.get('/api/books/:_id', (req, res) => {
+	Book.getBookById(req.params._id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
 });
 
-app.post('/api/books/:_id', function(req, res){
-    let book = req.body;
-    Book.addBook(book, function(err, book){
-      if(err){
-        throw err;
-      }
-      res.json(book);
-    });
+app.post('/api/books', (req, res) => {
+	var book = req.body;
+	Book.addBook(book, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
 });
 
-app.put('/api/books/:_id', function(req, res){
-  let id = req.params._id;
-    let book = req.body;
-    Book.updateBook(id, book, {}, function(err, book){
-      if(err){
-        throw err;
-      }
-      res.json(book);
-    });
+app.put('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	var book = req.body;
+	Book.updateBook(id, book, {}, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
 });
 
-app.delete('/api/books/:_id', function(req, res){
-  let id = req.params._id;
-    Book.removeBook(id, function(err, book){
-      if(err){
-        throw err;
-      }
-      res.json(book);
-    });
+app.delete('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	Book.removeBook(id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
 });
 
 //Testing Purposes
