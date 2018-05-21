7cdae5929e1b62e0b2cb8045f7e567434bd47845
Matt <mattvroom1@gmail.com>
2018-05-21 18:59:19 -0500

    updates

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -3,11 +3,11 @@
 myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
   console.log('BooksController loaded');
 
-  $scope.getBooks = function(){
-    $http.get('/api/books').then(function(response){
-      $scope.books = response.data;
-    });
-  }
+  // $scope.getBooks = function(){
+  //   $http.get('/api/books').then(function(response){
+  //     $scope.books = response.data;
+  //   });
+  // }
 
   $scope.getBook = function(){
     let id = $routeParams.id;
